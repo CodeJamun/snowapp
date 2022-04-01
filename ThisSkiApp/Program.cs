fb79e6a7b67072d431f624072d1e4c6b16821c31
rannie <codejamun@gmail.com>
2022-03-31 21:23:18 -0400

    * .gitignore: * ThisSkiApp.sln: * ThisSkiApp/Program.cs:   asdf
    * ThisSkiApp/ThisSkiApp.csproj:
      Yay!!

---
+++ ThisSkiApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThisSkiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
